bb157d6faf36de4528e9d985cc823a1d90c00d86
Juana Paiva <juanapaiva2019@gmail.com>
2020-08-03 01:14:24 -0300

    + Video registration

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,11 +1,63 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import useForm from '../../../hooks/useForm';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
 
 function CadastroVideo() {
+  const history = useHistory();
+  const { values, handleChange } = useForm({
+    titulo: 'SORVETE VEGANO',
+    url: 'https://www.youtube.com/watch?v=divlj_H8BE0',
+    categoria: 'Receitas Veganas',
+  });
+
   return (
     <PageDefault>
       <h1>Cadastro de Vídeo</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+
+        videosRepository.createVideo({
+          titulo: values.titulo,
+          url: values.url,
+          categoriaId: 6,
+        })
+          .then(() => {
+            console.log('Cadastrou com sucesso!');
+            history.push('/');
+          });
+      }}
+      >
+        <FormField
+          label="Título do vídeo"
+          type="text"
+          name="titulo"
+          value={values.titulo}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="URL do vídeo"
+          type="url"
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Categoria"
+          type="text"
+          name="categoria"
+          value={values.categoria}
+          onChange={handleChange}
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
 
       <Link to="/cadastro/categoria">
         Cadastrar categoria
