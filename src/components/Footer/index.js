e8e96dc8cbc19f43459c1678a5d554aaedaa685a
Juana Paiva <juanapaiva2019@gmail.com>
2020-08-03 16:50:52 -0300

    Footer style change

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import FooterBase from './styles';
-import Logo from '../../assets/images/logo-rabanetflix.png';
+import Linkedin from '../../assets/images/linkedin.svg';
+import Github from '../../assets/images/github.svg';
+import Instagram from '../../assets/images/instagram.svg';
+import Facebook from '../../assets/images/facebook.svg';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="/">
-        <img className="Logo" src={Logo} alt="Rabanetflix logo" />
-      </a>
+      <div>
+        <a href="https://www.linkedin.com/in/juana-paiva/">
+          <img className="Logo" src={Linkedin} alt="Facebook" />
+        </a>
+        <a href="https://www.instagram.com/jubafuu/">
+          <img className="Logo" src={Github} alt="Facebook" />
+        </a>
+        <a href="https://github.com/NyraTyto">
+          <img className="Logo" src={Instagram} alt="Facebook" />
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=100008840581765">
+          <img className="Logo" src={Facebook} alt="Facebook" />
+        </a>
+      </div>
       <p>
-        &copy; Criado por
-        {' '}
-        <a href="https://www.linkedin.com/in/juana-paiva/">Juana Paiva</a>
-        {' '}
-        durante a
+        Projeto criado durante a
         {' '}
         <a href="https://www.alura.com.br/">Imersão React da Alura</a>
       </p>
+      <p>&copy; 2020. Juana Paiva</p>
     </FooterBase>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -7,8 +7,27 @@ const FooterBase = styled.footer`
   padding-right: 16px;
   padding-top: 32px;
   padding-bottom: 32px;
-  color: var(--white);
+  color: #919191;
   text-align: center;
+
+  & img {
+    width: 35px;
+    margin: 0 7.5px;
+    transition: .3s;
+  }
+  & img:hover {
+    width: 40px;
+  }
+
+  & a {
+    text-decoration: none;
+    font-weight: bold;
+    transition: .3s;
+  }
+  & a:hover {
+    color: #c4c4c4;
+  }
+
   @media (max-width: 800px) {
     margin-bottom: 50px;
   }
